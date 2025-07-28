55c6868a59f6be71124b953c3f901d41f582f790
juliao <juliaonkuna@gmail.com>
2025-07-28 15:07:27 +0200

    ajustar a funçao

--- ex03/ft_recursive_power.c
+++ ex03/ft_recursive_power.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_recursive_power.c                               :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jucongol <jucongol@student.42.fr>          #+#  +:+       +#+        */
+/*   By: jcongolo <jcongolo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025-07-23 19:11:24 by jucongol          #+#    #+#             */
-/*   Updated: 2025-07-23 19:11:24 by jucongol         ###   ########.fr       */
+/*   Created: 2025/07/23 19:11:24 by jucongol          #+#    #+#             */
+/*   Updated: 2025/07/28 15:06:29 by jcongolo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,13 +34,9 @@ int ft_recursive_power(int nb, int power)
     {
         return(0);
     }
-    else if(power == 0)
+    else if(power == 0 && nb == 0)
     {
         return(1);
-    }
-    else if(nb == 0)
-    {
-        return(0);
     }
     return(nb * (ft_recursive_power(nb, power - 1)));
     // 3 * (ft_recursive_power(3, 2));
